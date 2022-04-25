52261ed047d8dd5ba1bd9154b797bd50c7d89997
Mauricio Amador <94086257+MauwiA3@users.noreply.github.com>
2022-04-25 11:38:57 -0400

    Update valida_reg_log.js
    validando con jquery

--- valida_reg_log.js
+++ valida_reg_log.js
@@ -16,3 +16,44 @@
             }, false)
         })
 })()
+
+$(document).ready(function(){
+    $("$registro_for").validate({
+        rules:{
+            nombre:{
+                required: true,
+                minlenght: 10
+            },
+            contraseña:{
+                required: true,
+                minlenght: 10
+            },
+
+            contraseña2:{
+                required: true,
+                minlenght: 10
+            },
+            
+            correo:{
+                required: true,
+                email:true
+            }
+        },
+        messages:{
+            nombre:{
+                required:"El nombre de ususario es obligatorio",
+                minlenght: "es necesario 10 caracteres"
+            },
+
+            contraseña:{
+                required:"Contraseña requerida",
+                minlenght:"Minimo 6 caracteres"
+            },
+            correo:{
+                required:"El correo es resquerido",
+                minlenght:"El correo no es valido"
+            }
+        }
+    });
+
+});
