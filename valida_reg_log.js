cc5cf20c6fa35c62da0fd6eabd61d30d74aa83cb
Mauricio Amador <94086257+MauwiA3@users.noreply.github.com>
2022-04-25 12:53:28 -0400

    Update valida_reg_log.js

--- valida_reg_log.js
+++ valida_reg_log.js
@@ -17,32 +17,6 @@
         })
 })()
 
-//verificacion de contraseñas 
-function verificarPasswords() {
-
-    pass1 = document.getElementById('pass1');
-    pass2 = document.getElementById('pass2');
-
-    if (pass1.value != pass2.value) {
-
-        // Si las constraseñas no coinciden mostramos un mensaje 
-        document.getElementById("error").classList.add("mostrar");
-        return false;
-    }
-
-    else {
-        document.getElementById("error").classList.remove("mostrar");
-        document.getElementById("ok").classList.remove("ocultar");
-
-        document.getElementById("registro").disabled = true;
-
-        setTimeout(function () {
-            location.reload();
-        }, 3000);
-        return true;
-    }
-}
-
 $(document).ready(function(){
     $("$registro_for").validate({
         rules:{
