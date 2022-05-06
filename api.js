d64165e2cfa0cabc8aa8d6b1566d7ea3789911d8
MauwiA3 <94086257+MauwiA3@users.noreply.github.com>
2022-05-05 23:59:11 -0400

    css de api en style principal

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 let url = 'https://jsonplaceholder.typicode.com/users/';
-<<<<<<< HEAD
+
 fetch(url)
     .then(response => response.json())
     .then(data => mostrarData(data))
@@ -13,19 +13,3 @@ const mostrarData = (data) => {
     }
     document.getElementById('data').innerHTML = body
 }
-=======
-        fetch(url)
-            .then( response => response.json() )
-            .then( data => mostrarData(data) )
-            .catch( error => console.log(error) )
-
-        const mostrarData = (data) => {
-            console.log(data)
-            let body = ""
-            for (var i = 0; i < data.length; i++) {      
-               body+=`<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`
-            }
-            document.getElementById('data').innerHTML = body
-            //console.log(body)
-        }
->>>>>>> cc5cf20c6fa35c62da0fd6eabd61d30d74aa83cb
